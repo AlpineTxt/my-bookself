b3c58690673b54e327b650fc391edb6b9c41dde2
Pritam ___ <prit76415@gmail.com>
2026-05-22 23:07:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const books = [
     {
         id: 3,
         title: "WHY WE STAY WHY WE LEAVE",
-        author: "PRITAM B",
+       
         cover: "WHY WE STAY WHY WE LEAVE.png",
         flipbookUrl: "https://heyzine.com/flip-book/3039b6c5cc.html",
         buyUrl: "https://www.amazon.com/dp/B0H1D2MPC7"
